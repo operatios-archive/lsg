6795bfc9a6d3c1e7de4432d99917a96e290b754c
Operatios <satoyoshiro48@gmail.com>
2020-12-02 14:07:36 +0300

    Fix for issue #1

--- util_linux.go
+++
@@ -1,5 +0,0 @@
-package main
-
-func enableColors() error {
-	return nil
-}
---
+++ util_unix.go
@@ -0,0 +1,7 @@
+// +build darwin dragonfly freebsd linux netbsd openbsd
+
+package main
+
+func enableColors() error {
+	return nil
+}
